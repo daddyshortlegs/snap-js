83805cf1eeba6715ab657678682b6c6e04cedfc6
andys <andysmith836@gmail.com>
2020-05-01 22:08:55 +0100

    Replace with loop

--- snappy.js
+++ snappy.js
@@ -1,20 +1,17 @@
 function makeSnappy(deck, outputter, player1, player2) {
 
+    function changePlayer(player) {
+        return player === player1 ? player2 : player1;
+    }
+
     function play() {
         let card;
         let player = player1;
 
-        card = deck.takeCard();
-        outputter.display(`${player} turned card '${card}'`);
-        player = player === player1 ? player2: player1;
-
-        card = deck.takeCard();
-        outputter.display(`${player} turned card '${card}'`);
-        player = player === player1 ? player2: player1;
-
-        card = deck.takeCard();
-        outputter.display(`${player} turned card '${card}'`);
-        player = player === player1 ? player2: player1;
+        while (card = deck.takeCard()) {
+            outputter.display(`${player} turned card '${card}'`);
+            player = changePlayer(player);
+        }
 
     }
 
